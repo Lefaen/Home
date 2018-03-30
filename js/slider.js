3ca1ff9a2b4cddf5dbe83c3ee7869efd6ecafc40
Lefaen <Lefaen@gmail.com>
2018-03-30 15:20:26 +0300

    автопрокрутка слайдера

--- js/slider.js
+++ js/slider.js
@@ -1,8 +1,9 @@
+var labels = document.getElementsByClassName("controls");
 function showSlide(n){
     var label = document.getElementById("checked");
     var img = document.getElementById("slideImage");
     label.removeAttribute("id");
-    var labels = document.getElementsByClassName("controls");
+    
     
     var result, i, elm;
     
@@ -15,3 +16,26 @@ function showSlide(n){
     result.setAttribute("id", "checked");
     img.setAttribute("src", "/images/slider/slide" + n.toString() + ".jpg");
 };
+
+window.onload = function () {
+    autoSlide(1);
+    setInterval(function () {
+        var i = 1;
+
+        autoSlide(i);
+    }, 3*1000*labels.length);
+
+};
+
+
+function autoSlide(i)
+{
+    for(var i; i<7; i++) {
+        (function (i) {
+            setTimeout(function () {
+                showSlide(i);
+                console.log(i);
+            }, 3*1000 * i);
+        })(i);
+    }
+}
